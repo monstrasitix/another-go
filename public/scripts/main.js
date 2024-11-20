42e97a4ced38fcadbedd26f8a5cd387f0306e7ea
monstrasitix <dsavostijanovs@yahoo.ie>
2024-11-20 17:01:25 +0000

    Public

--- public/scripts/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function toggleClass(id, className) {
-    const el = document.getElementById(id);
-    if (el instanceof HTMLElement) {
-        el.classList.toggle(className);
-    }
-}
